de51940b9da62b269e7eb8e43c5b95f9ccf4d076
Davron <davron211@gmail.com>
2024-09-04 15:20:32 +0500

    change log prefix

--- logger.go
+++ logger.go
@@ -149,7 +149,7 @@ var defaultLogFormatter = func(param LogFormatterParams) string {
 	if param.Latency > time.Minute {
 		param.Latency = param.Latency.Truncate(time.Second)
 	}
-	return fmt.Sprintf("[GIN] %v |%s %3d %s| %13v | %15s |%s %-7s %s %#v \n|Headers:%s \n%s",
+	return fmt.Sprintf("[GIN-UPDATED] %v |%s %3d %s| %13v | %15s |%s %-7s %s %#v \n|Headers:%s \n%s",
 		param.TimeStamp.Format("2006/01/02 - 15:04:05"),
 		statusColor, param.StatusCode, resetColor,
 		param.Latency,
